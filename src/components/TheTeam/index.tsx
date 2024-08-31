fff247123721b3ab6f5ba4ae6c28cab8d37920d7
Dan Visel <danvisel@archive.org>
2024-08-31 12:45:55 +0200

    removed collaborators

--- src/components/TheTeam/index.tsx
+++ src/components/TheTeam/index.tsx
@@ -6,7 +6,7 @@ import getCleanTeam from "@/lib/getTeam";
 const TheTeam = async () => {
   const team = await getCleanTeam();
   const theTeam = team.filter((x) => x.type === "team");
-  const collaborators = team.filter((x) => x.type === "collaborator");
+  // const collaborators = team.filter((x) => x.type === "collaborator");
   return (
     <div className={styles.teamWrapper}>
       <h2>The Team</h2>
@@ -48,7 +48,7 @@ const TheTeam = async () => {
           );
         })}
       </div>
-      <h2>Collaborators</h2>
+      {/* <h2>Collaborators</h2>
       <div>
         {collaborators.map((person, index) => (
           <div className={styles.person} key={index}>
@@ -71,7 +71,7 @@ const TheTeam = async () => {
             </div>
           </div>
         ))}
-      </div>
+      </div> */}
     </div>
   );
 };
